e57b789d6f002205834acdfd942133294cf74946
chamZZZ <mz80k2e_x1g@yahoo.co.jp>
2020-07-24 23:19:50 +0900

    カメラマネージャー、地面スプライト、コンテキスト追加

---
+++ chamGame00/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : SingletonMonoBehaviour<CameraManager>
+{
+	[SerializeField]
+	private GameObject targetObject = default;
+
+	[SerializeField]
+	private Vector3 offsetPos = default;
+
+    // Update is called once per frame
+    void Update()
+	{
+		if( targetObject != null)
+		{
+			transform.position = targetObject.transform.position + offsetPos;
+		}
+	}
+
+	public void SetCameraTarget(GameObject obj)
+	{
+		targetObject = obj;
+	}
+
+	public void SetOffsetCamera(Vector3 offset)
+	{
+		offsetPos = offset;
+	}
+}
---
+++ chamGame00/Assets/Scripts/PlayerContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Game/Create PlayerContext")]
+public class PlayerContext : ScriptableObject
+{
+	public int testData;
+}
