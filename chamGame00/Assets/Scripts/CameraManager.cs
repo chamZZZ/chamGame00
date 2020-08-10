6c9716e36dc69d381a8f2af1fac5ff78ec70897e
chamZZZ <mz80k2e_x1g@yahoo.co.jp>
2020-08-10 14:49:56 +0900

    カメラ　パラメータ制御追加

--- chamGame00/Assets/Scripts/CameraManager.cs
+++ chamGame00/Assets/Scripts/CameraManager.cs
@@ -10,14 +10,37 @@ public class CameraManager : SingletonMonoBehaviour<CameraManager>
 	[SerializeField]
 	private Vector3 offsetPos = default;
 
-    // Update is called once per frame
-    void Update()
+	[SerializeField]
+	private float distance = 15.0f; // distance from following object
+
+	[SerializeField]
+	private float polarAngle = 45.0f; // angle with y-axis
+
+	[SerializeField]
+	private float azimuthalAngle = 45.0f; // angle with x-axis
+
+
+	// Update is called once per frame
+	void Update()
 	{
 		if( targetObject != null)
 		{
-			transform.position = targetObject.transform.position + offsetPos;
+			var lookAtPos = targetObject.transform.position + offsetPos;
+			updatePosition(lookAtPos);
+			transform.LookAt(lookAtPos);
 		}
 	}
+
+	void updatePosition(Vector3 lookAtPos)
+	{
+		var da = azimuthalAngle * Mathf.Deg2Rad;
+		var dp = polarAngle * Mathf.Deg2Rad;
+		transform.position = new Vector3(
+			lookAtPos.x + distance * Mathf.Sin(dp) * Mathf.Cos(da),
+			lookAtPos.y + distance * Mathf.Cos(dp),
+			lookAtPos.z + distance * Mathf.Sin(dp) * Mathf.Sin(da));
+	}
+
 
 	public void SetCameraTarget(GameObject obj)
 	{
